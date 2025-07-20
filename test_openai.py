bb6a38870948ea48af0383f70d7d1e9a4cd90078
ShrishPremkrishna <shrish.premkrishna@gmail.com>
2025-07-20 12:09:57 -0700

    clearning main

--- test_openai.py
+++
@@ -1,9 +0,0 @@
-import os
-import openai
-from dotenv import load_dotenv; load_dotenv()
-
-client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-try:
-    print(client.models.list())
-except Exception as e:
-    print("OpenAI error:", e) 
